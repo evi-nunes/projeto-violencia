74fc85fffc2f5376fe6e5a99d6f65046a9502e07
Évila Paulino Nunes Vieira <66744375+evi-nunes@users.noreply.github.com>
2026-04-30 00:46:40 +0000

    adição accordion com ícones na seção reconhecer

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function sairRapido() {
   window.location.href = "https://www.google.com";
 }
+
+const botoes = document.querySelectorAll(".accordion-btn");
+
+botoes.forEach(function(botao) {
+  botao.addEventListener("click", function() {
+    const conteudo = botao.parentElement.querySelector(".accordion-conteudo");
+    conteudo.classList.toggle("aberto");
+    botao.classList.toggle("ativo");
+  });
+});
