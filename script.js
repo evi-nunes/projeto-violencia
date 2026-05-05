15966d0c7aeac45c74828b8764bcb05bf02da329
Évila Paulino Nunes Vieira <66744375+evi-nunes@users.noreply.github.com>
2026-05-05 20:57:17 +0000

    adição de hamburguer

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ botoes.forEach(function(botao) {
     botao.classList.toggle("ativo");
   });
 });
+
+const hamburguer = document.getElementById('hamburguer');
+const menuLinks = document.getElementById('menu-links');
+
+hamburguer.addEventListener('click', function() {
+  hamburguer.classList.toggle('ativo');
+  menuLinks.classList.toggle('aberto');
+});
+
+menuLinks.querySelectorAll('a').forEach(function(link) {
+  link.addEventListener('click', function() {
+    hamburguer.classList.remove('ativo');
+    menuLinks.classList.remove('aberto');
+  });
+});
